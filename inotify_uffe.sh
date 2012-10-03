2080cc28139441d0b5fdb0fc8a0b2ea304786f84
Pansar <pansar@pansar-server.The-Empire>
2012-10-03 08:28:38 +0200

    Merged uffe.sh with inotify_uffe.sh. Web page now updated every time log changes

--- inotify_uffe.sh
+++ inotify_uffe.sh
@@ -1,4 +1,15 @@
 #!/bin/bash
 while inotifywait -e modify /home/pansar/loggar/bsnet/#teambacon.log; do
-	tail -1 /home/pansar/loggar/bsnet/#teambacon.log | grep snafu | awk '{ printf "oloflolof: "; print }' > /home/pansar/irc/localhost/\&bitlbee/in
+
+#Send message to snafu
+tail -1 /home/pansar/loggar/bsnet/#teambacon.log | grep snafu | awk '{ printf "oloflolof: "; print }' > /home/pansar/irc/localhost/\&bitlbee/in
+
+#Update html page
+echo "<!DOCTYPE html>" > /home/pansar/public_html/uffe/index.html
+echo "<html><head><meta charset='utf-8'></head><body><pre>" >> /home/pansar/public_html/uffe/index.html
+echo "<p>Now with working links</p>" >> /home/pansar/public_html/uffe/index.html
+tail -n50 /home/pansar/loggar/bsnet/#teambacon.log | sed 's/snafu/<b><u>&<\/u><\/b>/' | sed -e 's|http[^ ]*|<a href="&">&</a>|g' | tac >> /home/pansar/public_html/uffe/index.html
+echo "</pre></body></html>" >> /home/pansar/public_html/uffe/index.html
+
+
 done
--- uffe.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-echo "<!DOCTYPE html>" > /home/pansar/public_html/uffe/index.html
-echo "<html><head><meta charset='utf-8'></head><body><pre>" >> /home/pansar/public_html/uffe/index.html
-echo "<p>Now with working links</p>" >> /home/pansar/public_html/uffe/index.html
-tail -n50 /home/pansar/loggar/bsnet/#teambacon.log | sed 's/snafu/<b><u>&<\/u><\/b>/' | sed -e 's|http[^ ]*|<a href="&">&</a>|g' >> /home/pansar/public_html/uffe/index.html
-echo "</pre></body></html>" >> /home/pansar/public_html/uffe/index.html
