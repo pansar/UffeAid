4b9e723f0a1ab34e5985232674e0720f3e87b35c
Pansar <pansar@pansar-server.The-Empire>
2012-10-03 19:03:34 +0200

    Stoped messages from beeing send if snafu is active in the channel

--- inotify_uffe.sh
+++ inotify_uffe.sh
@@ -2,11 +2,11 @@
 while inotifywait -e modify /home/pansar/loggar/bsnet/#teambacon.log; do
 
 #Send message to snafu
-tail -1 /home/pansar/loggar/bsnet/#teambacon.log | grep snafu | awk '{ printf "oloflolof: "; print }' > /home/pansar/irc/localhost/\&bitlbee/in
+tail -1 /home/pansar/loggar/bsnet/#teambacon.log | sed 's/<+snafu>//g' | sed 's/\*\ snafu//g'  | grep snafu | awk '{ printf "oloflolof: "; print }' > /home/pansar/irc/localhost/\&bitlbee/in
 
 #Update html page
 echo "<!DOCTYPE html>" > /home/pansar/public_html/uffe/index.html
-echo "<html><head><meta charset='utf-8'></head><body><pre>" >> /home/pansar/public_html/uffe/index.html
+echo "<html><head><meta charset='utf-8'><meta http-equiv="refresh" content="6"></head><body><pre>" >> /home/pansar/public_html/uffe/index.html
 echo "<p>Now with working links</p>" >> /home/pansar/public_html/uffe/index.html
 tail -n50 /home/pansar/loggar/bsnet/#teambacon.log | sed 's/snafu/<b><u>&<\/u><\/b>/' | sed -e 's|http[^ ]*|<a href="&">&</a>|g' | tac >> /home/pansar/public_html/uffe/index.html
 echo "</pre></body></html>" >> /home/pansar/public_html/uffe/index.html
