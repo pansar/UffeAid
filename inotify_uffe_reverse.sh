b88b1451a8c731379cb87893a01492668135d866
Pansar <pansar@pansar-server.The-Empire>
2012-12-21 14:17:32 +0100

    Some changes

--- inotify_uffe_reverse.sh
+++ inotify_uffe_reverse.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 while inotifywait -e modify /home/pansar/irc/localhost/\&bitlbee/out; do
-        tail -1 /home/pansar/irc/localhost/\&bitlbee/out | grep "#tb" | sed -n 's/.*pansar://p' | sed 's/#tb//' > /home/pansar/irc/irc.bsnet.se/\#teambacon/in
+        tail -1 /home/pansar/irc/localhost/\&bitlbee/out | grep '<oloflolof> pansar:' | sed -n 's/.*pansar: //p' | sed 's/#tb//' > /home/pansar/irc/irc.bsnet.se/\#teambacon/in
 done
 
